94160084ae06aaf1eee4fceac380984d8035226d
Felix Odete <felix.odette@gmail.com>
2023-02-11 09:12:59 +0300

    Config: Home component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
 import { Header, Home } from './components';
 import { Route, Routes } from 'react-router-dom';
-
 function App() {
   return (
-    <div className="w-screen h-auto  flex flex-col bg-primary">
+    <div className="w-screen h-auto flex flex-col bg-primary">
       <Header />
       <main className='mt-24 p-8 w-full'>
+
         {/* Routes */}
         <Routes>
           <Route path="/*" element={<Home />} />
         </Routes>
+
       </main>
     </div>
   );
